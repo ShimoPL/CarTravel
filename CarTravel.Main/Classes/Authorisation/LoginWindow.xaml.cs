ed9ea3ea67d6298284f65a6df77513aaa3d83d6e
Szymon Kukla <Szymon Kukla>
2016-08-30 00:25:38 +0200

    Repo start

---
+++ CarTravel.Main/Classes/Authorisation/LoginWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CarTravel.Main
+{
+    /// <summary>
+    /// Interaction logic for LoginWindow.xaml
+    /// </summary>
+    public partial class LoginWindow : Window
+    {
+        public string login
+        {
+            get { return loginBox.Text; }
+            set { loginBox.Text = value; }
+        }
+
+        public string pass
+        {
+            get { return passBox.Password; }
+        }
+        public LoginWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void WindowLoaded(object sender, EventArgs e)
+        {
+            if (loginBox.Text == "")
+                wrongLabel.Visibility = Visibility.Collapsed;
+            else
+                wrongLabel.Visibility = Visibility.Visible;
+        }
+
+        private void loginBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (loginBox.Text != "" && passBox.Password != "")
+                DialogResult = true;
+            else return;
+        }
+
+        private void closeButton_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
---
+++ CarTravel.Main/Classes/Authorisation/UserLogOn.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace CarTravel.Main.Authorisation
+{
+    class UserLogOn
+    {
+        public users ActualUser { get; private set; }
+        public bool IsLogged { get; private set; }
+        public UserLogOn()
+        {
+            int userId = 0;
+            string login = "";
+            do
+            {
+                var loginWin = new LoginWindow
+                {
+                    login = login
+                };
+                loginWin.ShowDialog();
+
+                if (loginWin.DialogResult == true)
+                {
+                    login = loginWin.login;
+                    userId = CheckUserCredentials(login, loginWin.pass);
+                }
+
+                if (userId != -1)
+                {
+                    ActualUser = GetUserInfo(userId);
+                    if (ActualUser.role != "E")
+                    {
+                        var loginAgain = System.Windows.MessageBox.Show("You are not permited to use management system!\nDo You want login to another accout?", "User not permited", System.Windows.MessageBoxButton.YesNo, System.Windows.MessageBoxImage.Exclamation, System.Windows.MessageBoxResult.Yes);
+                        if (loginAgain == System.Windows.MessageBoxResult.Yes) userId = -1;
+                        else Application.Current.Shutdown();
+                    }
+                }
+            } while (userId == -1);
+
+        }
+
+        private int CheckUserCredentials(string login, string pass)
+        {
+            using (var db = new CarTravelDb())
+            {
+                var user = db.systemusers.Where(l => l.login == login && l.password == pass).FirstOrDefault();
+                return user != null ? user.userId : -1;
+            }
+        }
+
+        private users GetUserInfo(int userId)
+        {
+            using (var db = new CarTravelDb())
+            {
+                return db.users.Where(u => u.userId == userId).FirstOrDefault();
+            }
+        }
+
+    }
+}
